9521fad777ec37d2bfa055e19a0addaa8de40921
AnthonyFranzino <afranzino@ufl.edu>
2023-11-01 14:04:17 -0400

    anthony

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-def menu():
-    user_selection = ""
+def decode_password(encoded):
+    password = ""
+    for num in str(encoded):
+        new_num = str((int(num) - 3) % 10)
+        password += new_num
+    return password
 
-print("Menu")
-print("-------------")
-print("1. Encode")
-print("2. Decode")
-print("3. Quit")
+
+def menu():
+
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+    user_selection = int(input("Please enter an option: "))
+
+    if user_selection == 2:
+        decoded = decode_password(encoded)
+        print("The encoded password is " + encoded + ", and the original password is " + decoded + ".")
